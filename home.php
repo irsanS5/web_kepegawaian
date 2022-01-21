e8b107fd9712a59825e7d3b5a6039bf38eb53d17
irsanS5 <irsanmaularifli@gmail.com>
2022-01-21 15:37:49 +0800

    first commit

---
+++ home.php
@@ -0,0 +1,248 @@
+
+<?php
+        include 'inc/koneksi.php';
+        $sql_cek = "SELECT * FROM tb_profil";
+        $query_cek = mysqli_query($koneksi, $sql_cek);
+		$data_cek = mysqli_fetch_array($query_cek,MYSQLI_BOTH);
+		
+
+		
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Kepegawaian Bumigora</title>
+	<link rel="icon" href="dist/img/logo.png">
+	<!-- Tell the browser to be responsive to screen width -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+	<!-- Ionicons -->
+	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+	<!-- DataTables -->
+	<link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+	<!-- overlayScrollbars -->
+	<link rel="stylesheet" href="dist/css/adminlte.min.css">
+	<!-- Select2 -->
+	<link rel="stylesheet" href="plugins/select2/css/select2.min.css">
+	<link rel="stylesheet" href="plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+	<!-- Google Font: Source Sans Pro -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+	<!-- Alert -->
+	<script src="plugins/alert.js"></script>
+</head>
+
+<body>
+	<!-- Site wrapper -->
+	<!-- <div class="wrapper"> -->
+		<!-- Navbar -->
+		<!-- <nav class="main-header navbar navbar-expand navbar-blue navbar-light"> -->
+			<!-- Left navbar links -->
+			<!-- <ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" data-widget="pushmenu" href="#">
+						<i class="fas fa-bars text-white"></i>
+					</a>
+				</li>
+
+			</ul> -->
+
+			<!-- SEARCH FORM -->
+			<!-- <ul class="navbar-nav ml-auto">
+
+				<li class="nav-item d-none d-sm-inline-block">
+					<a href="index.php" class="nav-link">
+						<font color="white">
+							<b>
+							</b>
+						</font>
+					</a>
+				</li>
+			</ul>
+
+		</nav> -->
+		<!-- /.navbar -->
+
+		<!-- Main Sidebar Container -->
+
+		<!-- Content Wrapper. Contains page content -->
+		<!-- <div class="content-wrapper"> -->
+			<!-- Content Header (Page header) -->
+			<section class="content-header">
+			</section>
+
+			<!-- Main content -->
+			<section class="content">
+				<!-- /. WEB DINAMIS DISINI ############################################################################### -->
+				<div class="container-fluid">
+                    <div>
+                    <!-- <button class="btn btn-danger" name="login">
+                        <a href="login.php"></a>
+					<i class="fa fa-user"></i> Login</a>
+                    </button> -->
+				<a href="login.php" class="btn btn-danger" name="login">
+					<i class="fa fa-user"></i> Login</a>
+			</div>
+
+                
+            <br><br>
+
+                <div class="card card-info">
+	<div class="card-header">
+		<h3 class="card-title">
+			<i class="fa fa-flag"></i> Profil Perusahaan</h3>
+	</div>
+	<form action="" method="post" enctype="multipart/form-data">
+		<div class="card-body">
+			<div class="form-group row">
+				<label class="col-sm-2 col-form-label">Nama Perusahaan</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" id="nama_profil" name="nama_profil" value="<?php echo $data_cek['nama_profil']; ?>"
+					 readonly/>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-sm-2 col-form-label">Alamat</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" id="alamat" name="alamat" value="<?php echo $data_cek['alamat']; ?>"
+					 readonly/>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-sm-2 col-form-label">Bidang</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" id="bidang" name="bidang" value="<?php echo $data_cek['bidang']; ?>"
+					 readonly/>
+				</div>
+			</div>
+
+		</div>
+	</form>
+</div>
+
+<div class="card card-info">
+	<div class="card-header">
+		<h3 class="card-title">
+			<i class="fa fa-flag"></i> Data Karyawan</h3>
+	</div>
+	<form action="" method="post" enctype="multipart/form-data">
+		<div class="card-body">
+        <table id="example1" class="table table-bordered table-striped">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Foto</th>
+						<th>NIP</th>
+						<th>Nama</th>
+						<th>Jabatan</th>
+					</tr>
+				</thead>
+				<tbody>
+
+					<?php
+              $no = 1;
+			  $sql = $koneksi->query("SELECT * from tb_pegawai");
+              while ($data= $sql->fetch_assoc()) {
+            ?>
+
+					<tr>
+						<td>
+							<?php echo $no++; ?>
+						</td>
+						<td align="center">
+							<img src="foto/<?php echo $data['foto']; ?>" width="70px" />
+						</td>
+						<td>
+							<?php echo $data['nip']; ?>
+						</td>
+						<td>
+							<?php echo $data['nama']; ?>
+						</td>
+						<td>
+							<?php echo $data['jabatan']; ?>
+						</td>
+					</tr>
+
+					<?php
+              }
+            ?>
+				</tbody>
+				</tfoot>
+			</table>
+
+		</div>
+	</form>
+</div>
+
+				<!-- </div> -->
+			</section>
+			<!-- /.content -->
+		<!-- </div> -->
+		<!-- /.content-wrapper -->
+
+		
+
+		<!-- Control Sidebar -->
+		<aside class="control-sidebar control-sidebar-dark">
+			<!-- Control sidebar content goes here -->
+		</aside>
+		<!-- /.control-sidebar -->
+	</div>
+	<!-- ./wrapper -->
+
+	<!-- jQuery -->
+	<script src="plugins/jquery/jquery.min.js"></script>
+	<!-- Bootstrap 4 -->
+	<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<!-- Select2 -->
+	<script src="plugins/select2/js/select2.full.min.js"></script>
+	<!-- DataTables -->
+	<script src="plugins/datatables/jquery.dataTables.js"></script>
+	<script src="plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+	<!-- AdminLTE App -->
+	<script src="dist/js/adminlte.min.js"></script>
+	<!-- AdminLTE for demo purposes -->
+	<script src="dist/js/demo.js"></script>
+	<!-- page script -->
+	<script src="plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+	<script src="plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+	<script src="plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+	<script src="plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+	<script src="plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+	<script src="plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+	<script src="plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+
+	<script>
+		$(function() {
+			$("#example1").DataTable();
+			$('#example2').DataTable({
+				"paging": true,
+				"lengthChange": false,
+				"searching": false,
+				"ordering": true,
+				"info": true,
+				"autoWidth": false,
+			});
+		});
+	</script>
+
+	<script>
+		$(function() {
+			//Initialize Select2 Elements
+			$('.select2').select2()
+
+			//Initialize Select2 Elements
+			$('.select2bs4').select2({
+				theme: 'bootstrap4'
+			})
+		})
+	</script>
+
+</body>
+
+</html>
